aa0f1e833d11a9e9e329a5d17dd7984315817217
endliar <endliar@yandex.ru>
2023-08-26 19:51:28 +0700

    Доработ интерфейс успешной обработки ссылки.

--- frontend/shorten_link.php
+++ frontend/shorten_link.php
@@ -1,3 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Главная</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="create_link.php">Сократить ссылку</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<h1 class="text-center">Вы успешно сократили ссылку</h1>
 <?php
 
 use work_with_links\LinkShortener;
@@ -12,3 +41,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $linkShortener->generateLink($url);
 }
+?>
+</body>
+</html>
--- work_with_links/LinkShortener.php
+++ work_with_links/LinkShortener.php
@@ -26,32 +26,32 @@ class LinkShortener
         $stmt->execute();
     }
 
-//    public function getAllLinks() {
-//        $connection = $this->database->getConnection();
-//
-//        $stmt = $connection->query("SELECT * FROM urls");
-//
-//        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-//    }
-//
-//    public function deleteLink($id) {
-//        $connection = $this->database->getConnection();
-//
-//        $stmt = $connection->prepare("DELETE FROM urls WHERE id = :id");
-//        $stmt->bindParam(':id', $id);
-//        $stmt->execute();
-//    }
-//
-//    public function regenerateLinks($id) {
-//        $connection = $this->database->getConnection();
-//
-//        $newShortName = substr(md5(uniqid()), 0, 6);
-//
-//        $stmt = $connection->prepare("UPDATE urls SET short_url = :short_name WHERE id = :id");
-//        $stmt->bindParam(':short_name', $newShortName);
-//        $stmt->bindParam(':id', $id);
-//        $stmt->execute();
-//
-//        return "http://example.io/$newShortName";
-//    }
+    public function getAllLinks() {
+        $connection = $this->database->getConnection();
+
+        $stmt = $connection->query("SELECT * FROM urls");
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function deleteLink($id) {
+        $connection = $this->database->getConnection();
+
+        $stmt = $connection->prepare("DELETE FROM urls WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+    public function regenerateLinks($id) {
+        $connection = $this->database->getConnection();
+
+        $newShortName = substr(md5(uniqid()), 0, 6);
+
+        $stmt = $connection->prepare("UPDATE urls SET short_url = :short_name WHERE id = :id");
+        $stmt->bindParam(':short_name', $newShortName);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        return "http://example.io/$newShortName";
+    }
 }
